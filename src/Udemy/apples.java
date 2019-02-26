35dc7f64e0bd256f459b8194b76031059996a87d
prajwalpitlehra <prajwalpitlehra123@gmail.com>
2019-02-26 13:21:21 -0800

    My first repository

---
+++ src/Udemy/apples.java
@@ -0,0 +1,14 @@
+package Udemy;
+
+public class apples {
+
+	private static int ID = 10;
+	
+	
+	public void here (){
+		ID++;
+		System.out.println(ID);
+	}
+	
+	
+}
---
+++ src/Udemy/roid.java
@@ -0,0 +1,13 @@
+package Udemy;
+
+public class roid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		apples a1 = new apples();
+		a1.here();
+		apples a2 = new apples();
+		a2.here();
+	}
+
+}
